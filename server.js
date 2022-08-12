068290b3d24f83b2832ea17dd0c8b970ceec2a9b
oumaima <lakhalouma6@gmail.com>
2022-08-12 20:22:32 +0100

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/front_gestion'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/front_gestion/index.html'));
+});
+app.listen(process.env.PORT || 4200);
