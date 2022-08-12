553432372209a65c09069995ad848881d4c548fd
oumaima <lakhalouma6@gmail.com>
2022-08-12 22:00:47 +0100

    upp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  urlBackend:'https://freelancyback.herokuapp.com/',
-  urlplayement:'https://api.preprod.konnect.network/api/v2/payments/init-payment'
-
+  urlBackend:'https://managmentt.herokuapp.com/',
+ 
 };
