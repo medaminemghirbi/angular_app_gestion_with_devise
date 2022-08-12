a5d1fab82230785dac0ad75941d16fa84f0584dd
oumaima <lakhalouma6@gmail.com>
2022-08-12 22:18:24 +0100

    uppppp

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/front_gestion'));
+app.use(express.static(__dirname + '/dist/pfe-front-reworked'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/front_gestion/index.html'));
+        '/dist/pfe-front-reworked/index.html'));
 });
 app.listen(process.env.PORT || 4200);
